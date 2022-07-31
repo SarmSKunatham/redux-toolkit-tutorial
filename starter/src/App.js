5fd5e964a7558c9665606b7a97004bf221ceceaa
Sueptrakul Kunatham [Sarm] <87699705+SarmSKunatham@users.noreply.github.com>
2022-07-31 21:29:36 +0700

    Done

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,23 @@
+import Navbar from "./component/Navbar";
+import CartContainer from "./component/CartContainer";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./component/Modal";
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems])
+
+  return (
+    <>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+      <h2>Redux Toolkit</h2>
+    </>);
 }
 export default App;
